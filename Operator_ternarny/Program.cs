9e49d7b8c2d5a863818ab35af6766f2c6bde8b8b
Igor Matsko <igormazsko@mail.ru>
2022-08-14 09:27:59 +0300

    add Operator Ternarny

---
+++ Operator_ternarny/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int n = Convert.ToInt32(ReadLine());
+string answer = n % 2 == 0 ? "Yes" : "No";
+// WriteLine(n % 2 == 0 ? "Yes" : "No");
