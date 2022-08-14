c55c90d1a183ec224a24fc22a80baee857535dba
Igor Matsko <igormazsko@mail.ru>
2022-08-14 14:14:50 +0300

    Operator ternarny correct

--- Operator_ternarny/Program.cs
+++ Operator_ternarny/Program.cs
@@ -1,7 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Проверяем на чётность
 using static System.Console;
 Clear();
 WriteLine("Введите число: ");
 int n = Convert.ToInt32(ReadLine());
-string answer = n % 2 == 0 ? "Yes" : "No";
-// WriteLine(n % 2 == 0 ? "Yes" : "No");
+// string answer = n % 2 == 0 ? "Yes" : "No";
+WriteLine(n % 2 == 0 ? "Yes" : "No");
