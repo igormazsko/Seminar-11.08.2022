86e6aceb9eb34686dff81769287fc89d6165aafb
Igor Matsko <igormazsko@mail.ru>
2022-08-14 14:19:15 +0300

    add Comparison A or B

---
+++ Cpmparison_A_or_B/Program.cs
@@ -0,0 +1,10 @@
+﻿// Сравнение двух чисел
+using static System.Console;
+Clear();
+WriteLine("Введите число A: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число B: ");
+int b = Convert.ToInt32(ReadLine());
+string answer = a>b ? "A>B" : a<b ?  "A<B" : "A=B";
+// WriteLine(answer);
+WriteLine(a>b ? "A>B" : a<b ?  "A<B" : "A=B");
